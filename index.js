e7d4d03533a471f644762e69afb8548ac7c795b5
falconAutoEliott <80833322+falconAutoEliott@users.noreply.github.com>
2021-05-24 15:15:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var app     = express();
 var path    = require("path");
 
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/public'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 
