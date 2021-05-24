47393623b4572cfd780f7990075cb9537f978f0e
falconAutoEliott <80833322+falconAutoEliott@users.noreply.github.com>
2021-05-24 16:15:31 -0400

    Update main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,8 @@
 /* J-Roc Baby */
 var ctlrName = "Touch any key on your controller";
 
+var express = require("express");
+var app     = express();
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules'));
 
