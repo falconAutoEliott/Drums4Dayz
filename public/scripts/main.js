d3544cbc71d67e71e1e1519e8da0e2ba667bd89a
falconAutoEliott <80833322+falconAutoEliott@users.noreply.github.com>
2021-05-24 19:48:55 -0400

    still..

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,11 +1,6 @@
 /* J-Roc Baby */
 var ctlrName = "Touch any key on your controller";
 
-var express = require("express");
-var app     = express();
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/node_modules'));
-
 var navigator = require('jzz');
 
 navigator.requestMIDIAccess()
