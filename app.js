292b63af18dc77cc90dced2b4d51e2cc50978f38
falconAutoEliott <80833322+falconAutoEliott@users.noreply.github.com>
2021-05-24 09:52:49 -0400

    express

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
 let http = require('http');
 let fs = require('fs');
+const express = require('express');
+const app = new express();
 
-let handleRequest = (request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    fs.readFile('./index.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            respone.write('Whoops! File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
-};
-
-http.createServer(handleRequest).listen(8000); 
-
+app.get('/', function(request, response){
+    response.sendFile('index.html');
+});
