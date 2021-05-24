80d991882494fcae5efa14a3e47c37a3870e2b4e
falconAutoEliott <80833322+falconAutoEliott@users.noreply.github.com>
2021-05-24 15:00:46 -0400

    assets ref?? maybe

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ var express = require("express");
 var app     = express();
 var path    = require("path");
 
-
+app.use(express.static(__dirname + '/public'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/index.html'));
 });
+
 
 app.listen(process.env.PORT);
 
