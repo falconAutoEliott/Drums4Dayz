801f9f6f02633e3f56e5bb08258f2568bf79f67d
falconAutoEliott <80833322+falconAutoEliott@users.noreply.github.com>
2021-05-24 11:45:43 -0400

    yep

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ var path    = require("path");
 
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/index.html'));
-    res.sendFile(path.join(__dirname+'/style.css'));
-    res.sendFile(path.join(__dirname+'/main.js'));
 });
 
 app.listen(process.env.PORT);
