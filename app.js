a571fd4fec0c978bfe4469252b84e9445f3e23e7
falconAutoEliott <80833322+falconAutoEliott@users.noreply.github.com>
2021-05-24 01:35:59 -0400

    http..

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 /* J-Roc Baby */
 var ctlrName = "Touch any key on your controller";
 var navigator = require('jzz');
-var document = Document; 
+
+let http = require('http');
+let fs = require('fs');
+
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};
+
+http.createServer(handleRequest).listen(8000); 
 
 navigator.requestMIDIAccess()
     .then(onMIDISuccess, onMIDIFailure);
-
 
 function onMIDISuccess(midiAccess) {
     console.log(midiAccess);
