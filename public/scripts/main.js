37bb8308fef658892af2e5d29e52ab8ebec35eeb
falconAutoEliott <80833322+falconAutoEliott@users.noreply.github.com>
2021-05-24 22:13:02 -0400

    Update main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,9 +1,9 @@
 /* J-Roc Baby */
 var ctlrName = "Touch any key on your controller";
 
-var navigator = require('jzz');
+import { requestMIDIAccess } from 'jzz';
 
-navigator.requestMIDIAccess()
+requestMIDIAccess()
     .then(onMIDISuccess, onMIDIFailure);
 
 function onMIDISuccess(midiAccess) {
